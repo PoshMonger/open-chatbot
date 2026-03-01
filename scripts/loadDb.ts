cd06f1f23e56bb34ac3e277a8f04102a54971c90
PoshMonger <85910267+PoshMonger@users.noreply.github.com>
2026-03-01 14:39:57 -0500

    update package.json

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,17 +1,7 @@
-import {DataApiClient} from "@datastax/astra-db-ts";
-import {PupeteerWebBaseLoader} from "langchain/document_loaders/web/puppeteer";
+import {DataAPIClient} from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
 import OpenAI from "openai";
-import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 import "dotenv/config";
 
-const astraClient = new DataApiClient({
-    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
-    databaseId: process.env.ASTRA_DB_ID,
-    databaseRegion: process.env.ASTRA_DB_REGION,
-});
-
-const loader = new PupeteerWebBaseLoader("https://www.google.com");
-const docs = await loader.load();
-
-console.log(docs);
